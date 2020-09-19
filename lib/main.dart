0883c069f5ec913f9c3cdaaccd4abb12c8f1d7f0
Souvik Guria <54489090+devSouvik@users.noreply.github.com>
2020-09-19 21:52:43 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audio_cache.dart';
 
-void main() => runApp(XylophoneApp());
+void main() => runApp(MyApp());
 
-class XylophoneApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   void playSound(int soundNumber) {
     final player = AudioCache();
     player.play('note$soundNumber.wav');
@@ -30,14 +30,14 @@ class XylophoneApp extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
 
-              buildKey(color: Colors.red, soundNumber: 1),
-              buildKey(color: Colors.orange, soundNumber: 2),
-              buildKey(color: Colors.yellow, soundNumber: 3),
-              buildKey(color: Colors.green, soundNumber: 4),
-              buildKey(color: Colors.teal, soundNumber: 5),
-              buildKey(color: Colors.blue, soundNumber: 6),
-              buildKey(color: Colors.indigo, soundNumber: 7),
-              buildKey(color: Colors.purple, soundNumber: 8),
+                buildKey(color: Colors.red, soundNumber: 1,note: 'C'),
+              buildKey(color: Colors.orange, soundNumber: 2,note: 'D'),
+              buildKey(color: Colors.yellow, soundNumber: 3,note: 'E'),
+              buildKey(color: Colors.green, soundNumber: 4,note: 'F'),
+              buildKey(color: Colors.teal, soundNumber: 5,note: 'G'),
+              buildKey(color: Colors.blue, soundNumber: 6,note: 'A'),
+              buildKey(color: Colors.indigo, soundNumber: 7,note: 'B'),
+              buildKey(color: Colors.purple, soundNumber: 8,note: 'C'),
 
             ],
           ),
