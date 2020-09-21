cfbfdd3369c3ecba26be6f4d65182f7bf5c0c142
Souvik Guria <54489090+devSouvik@users.noreply.github.com>
2020-09-21 20:13:30 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -9,16 +9,23 @@ class MyApp extends StatelessWidget {
     player.play('note$soundNumber.wav');
   }
 
- Expanded buildKey({Color color, int soundNumber, String note}) {
+  Expanded buildKey({Color color, int soundNumber, String note}) {
     return Expanded(
       child: FlatButton(
         color: color,
         onPressed: () {
           playSound(soundNumber);
         },
-        child: Text(
-          '$note',
-          style: TextStyle(color: Colors.white),
+        child: RotatedBox(
+          quarterTurns: 1,
+          child: Text(
+            '$note',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 25,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
         ),
       ),
     );
@@ -33,16 +40,14 @@ class MyApp extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-
-                buildKey(color: Colors.red, soundNumber: 1,note: 'C'),
-              buildKey(color: Colors.orange, soundNumber: 2,note: 'D'),
-              buildKey(color: Colors.yellow, soundNumber: 3,note: 'E'),
-              buildKey(color: Colors.green, soundNumber: 4,note: 'F'),
-              buildKey(color: Colors.teal, soundNumber: 5,note: 'G'),
-              buildKey(color: Colors.blue, soundNumber: 6,note: 'A'),
-              buildKey(color: Colors.indigo, soundNumber: 7,note: 'B'),
-              buildKey(color: Colors.purple, soundNumber: 8,note: 'C'),
-
+              buildKey(color: Colors.red, soundNumber: 1, note: 'C'),
+              buildKey(color: Colors.orange, soundNumber: 2, note: 'D'),
+              buildKey(color: Colors.yellow, soundNumber: 3, note: 'E'),
+              buildKey(color: Colors.green, soundNumber: 4, note: 'F'),
+              buildKey(color: Colors.teal, soundNumber: 5, note: 'G'),
+              buildKey(color: Colors.blue, soundNumber: 6, note: 'A'),
+              buildKey(color: Colors.indigo, soundNumber: 7, note: 'B'),
+              buildKey(color: Colors.purple, soundNumber: 8, note: 'C'),
             ],
           ),
         ),
